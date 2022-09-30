2c812516111b0c6519a11b4499d9cffd8a4b163d
Winnie <luwinnie12@gmail.com>
2022-09-30 15:40:02 -0700

    updates

--- sliding-window/424-longest-repeating-char/424-ver-2.js
+++ sliding-window/424-longest-repeating-char/424-ver-2.js
@@ -11,6 +11,12 @@ var characterReplacement = function (s, k) {
   for (let end = 0; end < s.length; end++) {
     map[s[end]] = (map[s[end]] || 0) + 1;
     maxCount = maxCount > map[s[end]] ? maxCount : map[s[end]];
+    // if meet the first char that is different from the previous
+    // then end - start + 1 will be greater than maxCount;
+    // if end - start + 1 - maxCount > k, then means there is no more
+    // available flip;
+    // then we need to shrink the window
+    // and decrease the value of the removed char (which is the first letter at start)
     if (end - start + 1 - maxCount > k) {
       map[s[start]] -= 1;
       start++;
