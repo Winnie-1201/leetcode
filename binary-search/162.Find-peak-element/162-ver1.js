f3d100c12a136a24c21555a2da5373398ec32188
Winnie <luwinnie12@gmail.com>
2022-11-01 09:04:14 -0400

    finished

--- binary-search/162.Find-peak-element/162-ver1.js
+++ binary-search/162.Find-peak-element/162-ver1.js
@@ -21,3 +21,35 @@ var findPeakElement = function (nums) {
   // if length === 1: return 0
   return nums.length - 1;
 };
+
+// from solution:
+// recursive binary search
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  return helper(0, nums.length - 1, nums);
+};
+
+var helper = function (left, right, nums) {
+  if (left === right) return left;
+  let mid = Math.floor((left + right) / 2);
+  // if (nums[i] < nums[i+1]) then we only need to check i+1 and i+2
+  if (nums[mid] <= nums[mid + 1]) return helper(mid + 1, right, nums);
+  else return helper(left, mid, nums);
+};
+
+// iterate binary search
+var findPeakElement = function (nums) {
+  // return helper(0, nums.length - 1, nums)
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    // if (left === right) return left;
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+};
