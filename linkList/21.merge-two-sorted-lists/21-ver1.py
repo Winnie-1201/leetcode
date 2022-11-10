6c0ec3da1a775a64e45c680d4dbd9cfbc8469ff5
Winnie <luwinnie12@gmail.com>
2022-11-10 08:29:34 -0500

    solved

---
+++ linkList/21.merge-two-sorted-lists/21-ver1.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        temp = ListNode()
+        result = temp
+        # print(list)
+        if not list1 or not list2:
+            return list1 or list2
+        while list1 and list2:
+            # print(temp, list1, list2)
+            if list1.val >= list2.val:
+                temp.next = list2
+                temp = temp.next
+                list2 = list2.next
+            else:
+                temp.next = list1
+                temp = temp.next
+                list1 = list1.next
+        if not list1:
+            temp.next = list2
+        elif not list2:
+            temp.next = list1
+
+        return result.next
---
+++ linkList/234.palindrome-linkedlist/234-ver1.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+ #         find the mid
+        temp = ListNode()
+        temp.next = head
+        mid = temp
+        while temp and temp.next:
+            temp = temp.next.next
+            mid = mid.next
+        mid = mid.next
+
+
+#       reverse the mid
+        prev = None
+        curr = mid
+        nxt = curr
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        while head and prev:
+            print(head.val, prev.val)
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        # if not head and not mid: return True
+        return True
+
+# another way:
+        # slow, fast, prev = head, head, None
+        # #  find the mid
+        # while fast and fast.next:
+        #     slow, fast = slow.next, fast.next.next
+        # prev, slow, prev.next = slow, slow.next, None
+
+        # # reverse the second half:
+        # while slow:
+        #     slow.next, prev, slow = prev, slow, slow.next
+        # fast, slow = head, prev
+
+        # while slow:
+        #     if slow.val != fast.val: return False
+        #     slow, fast = slow.next, fast.next
+        # return True
