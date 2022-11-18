3ccb3c4925271417117b6079a8584db0a5357e8b
Winnie <luwinnie12@gmail.com>
2022-11-18 07:57:25 -0500

    solved

---
+++ binary-search/543.diameter-of-bt/543-ver1.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.result = 0
+
+        def helper(node):
+
+            if not node:
+                return 0
+
+            left = helper(node.left)
+            right = helper(node.right)
+
+            self.result = max(self.result, left+right)
+
+            return 1 + max(left, right)
+
+        helper(root)
+        return self.result
